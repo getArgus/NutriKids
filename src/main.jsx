e76c5232eeceafa0fbbca3f26fd473286d2ad987
IamThiago-IT <thiagodossantos315@gmail.com>
2023-06-25 00:22:19 -0300

    Update and rename main.tsx to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import Home from "./Pages/Home";
-import Menu from "./Pages/Menu";
+import "./styles/index.css";
 
-import "./index.css";
+import Home from './pages/Home';
+import Sobre from './pages/Sobre';
 
 const router = createBrowserRouter([
   {
@@ -16,12 +16,12 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: "/",
-    element: <Menu />,
+    path: "/Sobre",
+    element: <Sobre />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
